c13bfe3053f9fcdb51ddcca4a1939dd83e38a855
sehebj13 <72529424+sehebj13@users.noreply.github.com>
2020-10-15 19:41:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function quote_moon(){
+window.alert("Shoot for the moon because, Even if you miss, you will land among the stars");
+}
+function quote_chocolate(){
+window.alert("Anything is good if it's made of chocolate - Jo Brand");
+}
+function quote_dark(){
+window.alert("Stars cannot shine without darkness.");
+}
